add624717474311a522ed0c740cc4615e7ac6610
Emanuelly <manu.presteslopes10@gmail.com>
2026-03-23 14:43:40 -0300

    Initial commit

---
+++ manu04/src/main/java/com/example/manu04/Manu04Application.java
@@ -0,0 +1,13 @@
+package com.example.manu04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Manu04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Manu04Application.class, args);
+	}
+
+}
